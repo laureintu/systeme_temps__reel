b5dfa22b899ad60421a577388e06d9385d218c1a
Laure Inturrisi <laure.inturrisi@ipsa.fr>
2024-03-20 11:34:47 +0100

    trouve l'index d'une valeur dans un tableau

--- arraylist.c
+++ arraylist.c
@@ -1,7 +1,11 @@
-#include <stdio.h>
+x#include <stdio.h>
 
 int main(){
 	int tableau[5]= {4,7,1,9,15};
+	int a = 9;
 	for(int i = 0; i<5; i++){
-		printf("%d \n",tableau[i]);}
+		if(tableau[i]==a){
+			printf("le chiffre %d est à l'indice %d",a,i);
+		};
+	};
 };
